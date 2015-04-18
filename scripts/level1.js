2f8b75116d9483372464deae2c983ae8de937219
Sam Backus <sbackus@kayak.com>
2015-04-18 16:10:40 -0400

    add scripts folder

---
+++ scripts/level1.js
@@ -0,0 +1,44 @@
+// alert(state.history[0].variables.my_var)
+// if (state.history[0].variables.my_var == 0){
+// 	alert("test")
+// }
+// alert(state.history[0].variables.my_var);
+// state.history[0].variables.my_var = 1;
+
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var keys = [];
+
+var message = ""
+
+var width = 500, height = 400, speed = 5
+
+window.addEventListener("keydown", function(e){
+	keys[e.keyCode] = true;
+}, false);
+
+window.addEventListener("keyup", function(e){
+	delete keys[e.keyCode];
+}, false);
+
+
+function game(){
+	update();
+	render();
+}
+
+function update(){
+
+}
+
+function render(){
+	context.clearRect(0, 0, width, height);
+	
+	context.fillStyle = "red"
+	context.fillRect(40, 40, 40, 40)
+}
+
+setInterval(function(){
+	game();
+}, 1000/60)
