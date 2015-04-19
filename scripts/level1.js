8571c3394c733893711a065414689a8d105e4313
Sam Backus <sbackus@kayak.com>
2015-04-18 22:06:33 -0400

    add ship movement

--- scripts/level1.js
+++ scripts/level1.js
@@ -28,29 +28,81 @@ var contextBackground = document.getElementById("backgroundCanvas").getContext("
 
 var keys = [];
 
+var avatar = null;
+/*
+up - 38
+down - 40
+left - 37
+right - 39
+
+space - 32
+
+w - 87
+a - 65
+s - 83
+d - 68
+*/
+
 $(document).keydown(function(e){
-	console.log("keydown");
+	keys[e.keyCode ? e.keyCode : e.which] = true;
 });
 
+$(document).keyup(function(e){
+	delete keys[e.keyCode ? e.keyCode : e.which];
+});
+
+
+function player(image) {
+	this.image = image;
+	this.x =  width/2;
+	this.y =  height/2;
+	this.width =  40;
+	this.height =  40;
+	this.speed = 7;
+	this.draw =  function(){
+		contextPlayer.drawImage(this.image,this.x,this.y)
+	};
+	this.update = function(){
+		if(keys[38]) this.y-=this.speed;
+		if(keys[40]) this.y+=this.speed;
+		if(keys[37]) this.x-=this.speed;
+		if(keys[39]) this.x+=this.speed;
+
+		if(this.x < 0) this.x = 0;
+		if(this.y < 0) this.y = 0;
+		if(this.x > width-this.width) this.x = width-this.width;
+		if(this.y > height-this.height) this.y = height-this.height;
+	};
+	this.touching =  function(other){
+		return !(this.x > other.x + other.width ||
+			this.x + this.width < other.x ||
+			this.y > other.y + other.height ||
+			this.y + this.height < other.y);
+	};
+};
+
 function init(){
+	avatar = new player(images[0]);
 	loop();
-	contextPlayer.drawImage(images[0],10,10)
 }
 
 function update(){
-
+	avatar.update();
 }
 
 function render(){
 	contextBackground.clearRect(0,0,width,height)
+	contextPlayer.clearRect(0,0,width,height)
+
+	avatar.draw();
 }
 
 function loop(){
 	requestAnimFrame(function(){
 		loop();
 	});
-	// update();
-	// render();
+	update();
+	render();
 }
 
 function loadImages(paths){
